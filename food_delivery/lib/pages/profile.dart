714f94d822b824b6c58e93082e7875706edb77fe
Bhudevi Dobbala <bhudevi2461@gmail.com>
2025-04-29 17:01:22 +0530

    added logout for profile.

--- food_delivery/lib/pages/profile.dart
+++ food_delivery/lib/pages/profile.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:food_delivery/pages/login.dart';
+import 'package:food_delivery/service/widget_support.dart';
 
 class Profile extends StatefulWidget {
   const Profile({super.key});
@@ -8,8 +11,31 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+
+  signOut() async{
+    await FirebaseAuth.instance.signOut();
+    Navigator.push(context, MaterialPageRoute(builder: (context)=> LogIn()));
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text("Loged out!"), duration: Durations.short2,)
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text("Profile"),);
+    return Scaffold(body: Container(
+      margin: EdgeInsets.only(top: 20),
+      child: Column(
+        children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Profile", style: AppWidget.boldTextFieldStyles(),),
+          ],
+        ),
+        SizedBox(height: 40,),
+        IconButton(onPressed: signOut, icon: Icon(Icons.logout))
+      ],),
+    ),);
   }
 }
